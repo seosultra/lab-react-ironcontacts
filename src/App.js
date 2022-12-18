cf14f695cadc6b8f7338777984c9be4c85baa423
Sumia EOs <seosultra@gmail.com>
2022-12-18 14:03:56 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import contactsJson from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsJson.slice(0, 5));
+
+  const handleAddContact = () => {
+    if (contacts.length === contactsJson.length) {
+      return;
+    }
+
+    let randomContact =
+      contactsJson[Math.floor(Math.random() * contactsJson.length)];
+
+    while (contacts.includes(randomContact)) {
+      randomContact =
+        contactsJson[Math.floor(Math.random() * contactsJson.length)];
+    }
+
+    setContacts([...contacts, randomContact]);
+  };
+
+  const handleSortByName = () => {
+    const sortArray = contacts.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+
+    setContacts([...sortArray]);
+  };
+
+  const handleSortByPopularity = () => {
+    const sortArray = contacts.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+
+    setContacts([...sortArray]);
+  };
+
+  const handleRemove = (id) => {
+    const filterArr = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts([...filterArr]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleAddContact}>Add random contact</button>
+      <button onClick={handleSortByName}>Sort by name</button>
+      <button onClick={handleSortByPopularity}>Sort by popularity</button>
+
+      <table>
+        <thead>
+          <tr>
+            <td>
+              <h2>Picture</h2>
+            </td>
+            <td>
+              <h2>Name</h2>
+            </td>
+            <td>
+              <h2>Popularity</h2>
+            </td>
+            <td>
+              <h2>
+                Won <br /> Oscar
+              </h2>
+            </td>
+            <td>
+              <h2>
+                Won <br /> Emmy
+              </h2>
+            </td>
+            <td>
+              <h2>Actions</h2>
+            </td>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr key={contact.id}>
+                <td>
+                  <img src={contact.pictureUrl} alt={contact.name} />
+                </td>
+                <td>{contact.name}</td>
+                <td>
+                  {Math.round((contact.popularity + Number.EPSILON) * 100) /
+                    100}
+                </td>
+                <td>{contact.wonOscar && "🏆"}</td>
+                <td>{contact.wonEmmy && "🏆"}</td>
+                <td>
+                  <button onClick={() => handleRemove(contact.id)}>
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
